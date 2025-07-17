2a2e981bfa6ea1bd09e7cf50e5220b4ea6842681
utkarshup32 <utkarshdwivedi1879@gmail.com>
2025-07-17 22:58:11 +0530

    vertical order traversal

---
+++ Trees/BFS/vertical_order_traversal.cpp
@@ -0,0 +1,84 @@
+/*
+987. Vertical Order Traversal of a Binary Tree
+Hard
+
+Given the root of a binary tree, calculate the vertical order traversal of the binary tree.
+
+For each node at position (row, col), its left and right children will be at positions 
+(row + 1, col - 1) and (row + 1, col + 1) respectively. The root of the tree is at (0, 0).
+
+The vertical order traversal of a binary tree is a list of top-to-bottom orderings
+ for each column index starting from the leftmost column and ending on the rightmost column. 
+ There may be multiple nodes in the same row and same column. In such a case, sort these nodes by their values.
+
+Return the vertical order traversal of the binary tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[9],[3,15],[20],[7]]
+Explanation:
+Column -1: Only node 9 is in this column.
+Column 0: Nodes 3 and 15 are in this column in that order from top to bottom.
+Column 1: Only node 20 is in this column.
+Column 2: Only node 7 is in this column.
+Example 2:
+
+
+Input: root = [1,2,3,4,5,6,7]
+Output: [[4],[2],[1,5,6],[3],[7]]
+Explanation:
+Column -2: Only node 4 is in this column.
+Column -1: Only node 2 is in this column.
+Column 0: Nodes 1, 5, and 6 are in this column.
+          1 is at the top, so it comes first.
+          5 and 6 are at the same position (2, 0), so we order them by their value, 5 before 6.
+Column 1: Only node 3 is in this column.
+Column 2: Only node 7 is in this column.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,multiset<int>>> nodes;
+        queue<pair<TreeNode *,pair<int,int>>>q;
+        q.push({root,{0,0}});
+        while(!q.empty()){
+            auto p= q.front();
+            q.pop();
+            TreeNode * node=p.first;
+            int x=p.second.first;
+            int y=p.second.second;
+            nodes[x][y].insert(node->val);
+            if(node->left){
+                q.push({node->left,{x-1,y+1}});
+            }
+            if(node->right){
+                q.push({node->right,{x+1,y+1}});
+            }
+            
+            
+        }vector<vector<int>> ans;
+        for (auto a:nodes){
+            vector <int> col;
+            for(auto z: a.second ){
+                col.insert(col.end(),z.second.begin(),z.second.end());
+            }ans.push_back(col);
+        }return ans;
+        
+        
+    }
+};
