f008d35ae00e390626609556fe4bc0fed559a936
Utkarsh Dwivedi <127047064+utkarshup32@users.noreply.github.com>
2025-08-07 04:17:02 +0530

    Time: 7 ms (16.37%), Space: 16 MB (10.21%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> adj;
+        int n = edges.size();
+        for (int i = 0; i<n; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            vector<bool> visited(n, false);
+            if (adj.find(u) != adj.end() && adj.find(v) != adj.end() && bfs(u,v,adj,visited)) {
+                return edges[i];
+            }
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        return {};
+    }
+    bool bfs(int u, int v, unordered_map<int, vector<int>> &adj, vector<bool> &visited) {
+        visited[u] = true;
+        queue<int> q;
+        q.push(u);
+        while (!q.empty()) {
+            int a = q.front();
+            q.pop();
+            for (int &b : adj[a]) {
+                if (b == v) return true;
+                if (!visited[b]) {
+                    q.push(b);
+                    visited[b] = true;
+                }
+            }
+        }
+        return false;
+    }
+};
