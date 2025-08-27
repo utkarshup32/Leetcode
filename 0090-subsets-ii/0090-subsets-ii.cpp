f78486d4b4b2f3d3d9556545ec2f5c16157ac3e5
Utkarsh Dwivedi <127047064+utkarshup32@users.noreply.github.com>
2025-08-28 02:21:39 +0530

    Time: 5 ms (12.85%), Space: 14.9 MB (14.26%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,6 +1,7 @@
-class Solution {
+/*class Solution {
 public:
     void backtrack(int ind, vector<int>& nums, vector<int>& subset,vector<vector<int>> &ans){
+        
         ans.push_back(subset);
         for(int i=ind; i<nums.size(); i++){
             if(i > ind && nums[i] == nums[i-1])continue;
@@ -16,5 +17,36 @@ public:
         sort(nums.begin(), nums.end());
         backtrack(0, nums, subset, ans);
         return ans;
+    }
+};*/
+class Solution {
+public:
+    void subsetsWithDupUtil(vector<int>& nums, int idx, vector<int>& v, vector<vector<int>>& res, set<vector<int>>& us) {
+        if (idx >= nums.size()) {
+            return;
+        } 
+
+        v.push_back(nums[idx]);
+
+        auto it = us.find(v);
+
+        if (it == us.end()) {
+            res.push_back(v);
+            us.insert(v);
+        }
+        
+        subsetsWithDupUtil(nums, idx+1, v, res, us);
+        v.pop_back();
+        subsetsWithDupUtil(nums, idx+1, v, res, us);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        set<vector<int>> us;
+        vector<int> v;
+        res.push_back(v);
+        subsetsWithDupUtil(nums, 0, v, res, us);
+        return res;
     }
 };
