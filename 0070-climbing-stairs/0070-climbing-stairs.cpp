9f3ff3bfabc18a388163971ddad66556cddba477
Utkarsh Dwivedi <127047064+utkarshup32@users.noreply.github.com>
2025-09-17 23:46:49 +0530

    Time: 0 ms (100%), Space: 8.7 MB (18.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -10,6 +10,7 @@ public:
     int climb(int n,vector<int>dp){
         dp[0]=1;
         dp[1]=1;
+        if(dp[n] != -1) return dp[n];
         for(int i=2;i<=n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }return dp[n];
