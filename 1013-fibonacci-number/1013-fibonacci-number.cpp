9db73edfc02bd9090e3aca62e09a1eea5ac09e31
Utkarsh Dwivedi <127047064+utkarshup32@users.noreply.github.com>
2025-09-20 18:53:31 +0530

    Time: 2 ms (77.56%), Space: 7.9 MB (20.85%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        
-        if(n<=1 ) return n ;
-        return fib(n-1)+fib(n-2);
+        if (n == 0) return n;
+        int prev= 1;
+        int prev1=0;
+        for(int i=2;i<=n;i++){
+            int curr=prev+prev1;
+            prev1=prev;
+            prev=curr;
+        }
+        return prev;
     }
 };
