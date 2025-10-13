5f477e11fd333aa59c19d45c3c75d0c443fceb79
Utkarsh Dwivedi <127047064+utkarshup32@users.noreply.github.com>
2025-10-13 20:28:27 +0530

    Time: 2 ms (53.9%), Space: 16.1 MB (54.91%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    static array<int, 26> freq(string& s){
+        array<int, 26> ans{};
+        for(char c: s){
+            ans[c-'a']++;
+        }
+        return ans;
+    }
+    static vector<string> removeAnagrams(vector<string>& words) {
+        int n=words.size(), l=0, r=1;
+        vector<string> ans={words[0]};
+        array<int, 26> f0=move(freq(words[0]));
+        for( ; r<n; r++){
+            string s=words[r];
+            auto x=move(freq(s));
+            if (f0!=x){
+                ans.push_back(s);
+                l=r;
+                f0=x;
+            }
+        }
+        return ans;
+    }
+};
