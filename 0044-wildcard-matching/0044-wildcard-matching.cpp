e0969f7d12d254b9f27fb8b71654c2392cecdb36
Utkarsh Dwivedi <127047064+utkarshup32@users.noreply.github.com>
2025-10-25 20:18:35 +0530

    Time: 59 ms (11.46%), Space: 15.6 MB (52.82%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool f(int i,int j,string& s, string& p,vector<vector<int>>&dp){
+    /*bool f(int i,int j,string& s, string& p,vector<vector<int>>&dp){
         if(i<0 && j<0) return true;
         if(j<0 && i>=0 ) return false;
         if(i<0 && j>=0){
@@ -12,11 +12,30 @@ public:
         if(p[j]=='*') return dp[i][j]=f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp);
         return dp[i][j]=false;
 
-    }
-    bool isMatch(string s, string p) {
-        int n=s.size();
-        int m=p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));  
-        return f(n-1,m-1,s,p,dp);
+    }*/
+    bool isMatch(string text, string pattern) {
+        int n1 = text.size();
+        int n2 = pattern.size();
+        vector<vector<bool>> dp(n1+1, vector<bool>(n2+1, false));
+        dp[0][0] = true;
+        
+        for (int j = 1; j <= n2; j++) {
+            if (pattern[j - 1] == '*') dp[0][j] = dp[0][j - 1];
+        }
+        for (int i = 1; i <= n1; i++) {
+            dp[i][0] = false;
+        }
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (text[i - 1] == pattern[j - 1] || pattern[j - 1] == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pattern[j - 1] == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 };
