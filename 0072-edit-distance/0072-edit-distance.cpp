6bfbe027f213c02745c9c81f1c28cef6206a8e54
Utkarsh Dwivedi <127047064+utkarshup32@users.noreply.github.com>
2025-10-25 12:47:23 +0530

    Time: 7 ms (61%), Space: 13 MB (64.81%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,25 +3,20 @@ public:
     //Time: O(m*n), Space: O(m*n);
     int minDistance(string word1, string word2) {
         int m = word1.length(), n = word2.length();
-        vector<vector<int>> memo(m+1, vector<int>(n+1, -1));
-        return editDistance(word1, word2, m, n, memo);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i=0;i<=m;i++){
+            dp[i][0]=i;
+            
+        }for(int i=0;i<=n;i++){
+            dp[0][i]=i;
+            
+        }for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1[i-1]==word2[j-1])  dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+            }
+        }return dp[m][n];
     }
     
-    int editDistance(string& word1, string& word2, int m, int n, vector<vector<int>>& memo){
-        if(m == 0) return memo[m][n] = n;
-        if(n == 0) return memo[m][n] = m;
-        
-        if(memo[m][n] != -1)
-            return memo[m][n];
-        
-        if(word1[m-1] == word2[n-1]) 
-            return memo[m][n] = editDistance(word1, word2, m-1, n-1, memo);
-        else{
-            int insertChar = editDistance(word1, word2, m, n-1, memo);
-            int deleteChar = editDistance(word1, word2, m-1, n, memo);
-            int replaceChar = editDistance(word1, word2, m-1, n-1, memo);
-            
-            return memo[m][n] = 1 + min({insertChar, deleteChar, replaceChar});
-        }
-    }
+    
 };
