eb8771c3692d2ec1797fb1d26cf08716801965cc
Utkarsh Dwivedi <127047064+utkarshup32@users.noreply.github.com>
2026-02-15 23:12:41 +0530

    Time: 0 ms (100%), Space: 10.1 MB (22.87%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,23 +1,18 @@
+// bitset version
 class Solution {
 public:
     string addBinary(string a, string b) {
-        
-        int i=a.size()-1;
-        int j=b.size()-1;
-         
-         string ans="";
-         int Carry=0;
-
-         while( i>=0 || j>=0 || Carry){
-            int Sum=Carry;
-            
-             if(i>=0) Sum+=(a[i--]-'0');
-             if(j>=0) Sum+=(b[j--]-'0');
-              
-            ans=char((Sum%2)+'0') + ans;
-            Carry=Sum/2;
-         }
-
-         return ans;
+        const int l_a=a.length(), l_b=b.length();
+        const int M=max(l_a+1, l_b+1);
+        bitset<10001> A(a), B(b);
+        char C[M+1];
+        C[M]='\0';
+        int carry=0;
+        for(int i=0; i<M; i++){
+            int tmp=A[i]+B[i]+carry;
+            C[M-1-i]='0'+(tmp&1);
+            carry=tmp>1;
+        }
+        return (C[0]=='0') ? string(C+1):string(C);
     }
 };
